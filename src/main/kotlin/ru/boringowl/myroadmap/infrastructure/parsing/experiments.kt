08b2f6092b6b9d648fd840c394899a37ce55218b
R1k0523 <isergey555345@gmail.com>
2022-05-15 14:45:47 +0300

    parser for vacancies, resumes count

--- src/main/kotlin/ru/boringowl/myroadmap/infrastructure/parsing/experiments.kt
+++ src/main/kotlin/ru/boringowl/myroadmap/infrastructure/parsing/experiments.kt
@@ -36,13 +36,14 @@ object Parser {
         val res = hashMapOf<String, Int>()
         skills.forEach {
             val simPair = res.getSimilarityAndKey(it.key)
-           if (simPair.first > 0.7 && !exclusionWords.contains(it.key)) {
+            if (simPair.first > 0.7 && !exclusionWords.contains(it.key)) {
                 res[simPair.second!!] = it.value + (res[simPair.second!!] ?: 0)
             } else {
                 res[it.key] = it.value
             }
         }
-        return res.toList().filter{(_, v) -> v > 1}.sortedBy { (k, v) -> v}.map{ (k, v) -> Pair(k, v*100/(pages*perPage))}.reversed().toMap()
+        return res.toList().filter { (_, v) -> v > 1 }.sortedBy { (k, v) -> v }
+            .map { (k, v) -> Pair(k, v * 100 / (pages * perPage)) }.reversed().toMap()
     }
 
     private fun getVacancySkills(id: String): List<String> {
@@ -66,12 +67,54 @@ object Parser {
             }
         }
     }
+
+
+    fun getVacancyAndResumeCount(job: String): Pair<Int, Int> {
+        println(job)
+        val vacancyLink =
+            "https://hh.ru/search/vacancy?search_field=name&search_field=company_name&search_field=description&text=$job"
+        val resumeLink =
+            "https://hh.ru/search/resume?text=$job&exp_period=all_time&logic=normal&pos=full_text&fromSearchLine=false"
+        val resumeRegex = Regex("резюме у.*?соискател")
+        val vacancyRegex = Regex("section-3\">[\\d&nbsp;  ]*<.*?>ваканси")
+        val vacancies = getCount(vacancyLink, vacancyRegex)
+        val resumes = getCount(resumeLink, resumeRegex)
+
+        return Pair(vacancies, resumes)
+    }
+
+    fun getCount(link: String, regex: Regex): Int {
+        val (_, _, result) = link
+            .httpGet()
+            .responseString()
+        return when (result) {
+            is Result.Failure -> {
+                println(result.getException())
+                return -1
+            }
+            is Result.Success -> {
+                val data = result.get()
+                val res = regex.find(data)!!.value
+                println(res)
+                res.replace("section-3", "").filter { it.isDigit() }.toInt()
+            }
+        }
+    }
 }
 
 fun main() {
-    val s = Parser.getVacanciesForJob(Routes.ANDROID.name)
-    s.forEach { (t, u) ->
-        println("$t : $u%")
+//    val s = Parser.getVacanciesForJob(Routes.ANDROID.name)
+//    s.forEach { (t, u) ->
+//        println("$t : $u%")
+//    }
+    val values = hashMapOf<String, Double>()
+    listOf("игры").forEach {
+        val s = Parser.getVacancyAndResumeCount(it)
+        println(s)
+        values[it] = s.second/(s.first*10).toDouble()
+    }
+    values.toList().sortedBy { it.second }.toMap().forEach { (t, u) ->
+        println("$t : ${String.format("%.2f", u)} соискателя на вакансию")
     }
 }
 
