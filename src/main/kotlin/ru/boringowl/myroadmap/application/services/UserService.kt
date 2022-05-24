beb12ceceaa5af173301e290499fe410d94d9c37
R1k0523 <isergey555345@gmail.com>
2022-05-24 13:00:27 +0300

    test

--- src/main/kotlin/ru/boringowl/myroadmap/application/services/UserService.kt
+++ src/main/kotlin/ru/boringowl/myroadmap/application/services/UserService.kt
@@ -39,7 +39,7 @@ class UserService(
     fun updatePassword(id: UUID, data: RestorePasswordData) {
         val user: JpaUser? = userRepo.findById(id).orElse(null)
         println(user?.password.toString())
-        println(data.oldPassword)
+        println(passwordEncoder.encode(data.oldPassword))
         require(user?.password == passwordEncoder.encode(data.oldPassword)) {"Пароли не совпадают"}
         setUserPassword(id, data.newPassword)
     }
